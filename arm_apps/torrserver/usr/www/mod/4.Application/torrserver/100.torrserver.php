3b8265ab226e9c4beb922ce9d505a9e54ea23b6f
vladlenas <wotvladas@gmail.com>
2021-11-02 10:29:58 -0700

    First commit

---
+++ arm_apps/torrserver/usr/www/mod/4.Application/torrserver/100.torrserver.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <title>TorrServer</title>
+    
+</head>
+<body>
+    
+<h1>TorrServer</h1>
+<p>Find more information in website: <a href="https://github.com/YouROK/TorrServer"> </a></p>
+<p>TorrServer web http://NAS_IP:80890</p>
+</body>
+</html>
